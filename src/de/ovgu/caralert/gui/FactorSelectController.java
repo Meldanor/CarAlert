887abe465825eab88846416294366484b3f1cb9c
Nasto <stephan.doerfler@st.ovgu.de>
2014-01-23 12:27:36 +0100

    changed factorSelectionGUI to DropDown

--- src/de/ovgu/caralert/gui/FactorSelectController.java
+++ src/de/ovgu/caralert/gui/FactorSelectController.java
@@ -5,7 +5,10 @@ import com.jme3.asset.plugins.FileLocator;
 
 import de.lessvoid.nifty.Nifty;
 import de.lessvoid.nifty.NiftyEventSubscriber;
-import de.lessvoid.nifty.controls.RadioButtonGroupStateChangedEvent;
+import de.lessvoid.nifty.controls.DropDown;
+import de.lessvoid.nifty.controls.DropDownSelectionChangedEvent;
+import de.lessvoid.nifty.controls.dropdown.DropDownControl;
+import de.lessvoid.nifty.controls.label.builder.LabelBuilder;
 import de.lessvoid.nifty.screen.Screen;
 import de.lessvoid.nifty.screen.ScreenController;
 import eu.opends.main.Simulator;
@@ -14,7 +17,7 @@ public class FactorSelectController implements ScreenController{
 	
 	int brightnessFactor = 1;
 	int carClassFactor = 1;
-	int carStateFactor = 1;
+	int carSelectionFactor = 1;
 	int driverConditionFactor = 1;
 	int environmentFactor = 1;
 	int loadFactor = 1;
@@ -46,87 +49,90 @@ public class FactorSelectController implements ScreenController{
 	}
 
 
-	@NiftyEventSubscriber(id="brightnessRadioButtonGroup")
-	public void onBrightnessRadioButtonGroup(final String id, final RadioButtonGroupStateChangedEvent event){
-		this.brightnessFactor = Character.getNumericValue(event.getSelectedId().charAt(0));
+	@NiftyEventSubscriber(id="brightnessDropDown")
+	public void onBrightnessDropDown(final String id, final DropDownSelectionChangedEvent event){
+		this.brightnessFactor = nifty.getScreen("factor").findNiftyControl(id, DropDown.class).getSelectedIndex();
 	}
 
-	@NiftyEventSubscriber(id="carClassRadioButtonGroup")
-	public void onCarClassRadioButtonGroup(final String id, final RadioButtonGroupStateChangedEvent event){
-		this.carClassFactor = Character.getNumericValue(event.getSelectedId().charAt(0));
+	@NiftyEventSubscriber(id="carClassDropDown")
+	public void onCarClassDropDown(final String id, final DropDownSelectionChangedEvent event){
+		this.carClassFactor = nifty.getScreen("factor").findNiftyControl(id, DropDown.class).getSelectedIndex();
 	}
 
-	@NiftyEventSubscriber(id="carStateRadioButtonGroup")
-	public void onCarStateRadioButtonGroup(final String id, final RadioButtonGroupStateChangedEvent event){
-		this.carStateFactor = Character.getNumericValue(event.getSelectedId().charAt(0));
+	@NiftyEventSubscriber(id="carSelectionDropDown")
+	public void onCarSelectionDropDown(final String id, final DropDownSelectionChangedEvent event){
+		this.carSelectionFactor = nifty.getScreen("factor").findNiftyControl(id, DropDown.class).getSelectedIndex();
 	}
 
-	@NiftyEventSubscriber(id="driverConditionRadioButtonGroup")
-	public void onDriverConditionRadioButtonGroup(final String id, final RadioButtonGroupStateChangedEvent event){
-		this.driverConditionFactor = Character.getNumericValue(event.getSelectedId().charAt(0));
+	@NiftyEventSubscriber(id="driverConditionDropDown")
+	public void onDriverConditionDropDown(final String id, final DropDownSelectionChangedEvent event){
+		this.driverConditionFactor = nifty.getScreen("factor").findNiftyControl(id, DropDown.class).getSelectedIndex();
 	}
 
-	@NiftyEventSubscriber(id="environmentRadioButtonGroup")
-	public void onEnvironmentRadioButtonGroup(final String id, final RadioButtonGroupStateChangedEvent event){
-		this.environmentFactor = Character.getNumericValue(event.getSelectedId().charAt(0));
+	@NiftyEventSubscriber(id="environmentDropDown")
+	public void onEnvironmentDropDown(final String id, final DropDownSelectionChangedEvent event){
+		this.environmentFactor = nifty.getScreen("factor").findNiftyControl(id, DropDown.class).getSelectedIndex();
 	}
 
-	@NiftyEventSubscriber(id="loadRadioButtonGroup")
-	public void onLoadRadioButtonGroup(final String id, final RadioButtonGroupStateChangedEvent event){
-		this.loadFactor = Character.getNumericValue(event.getSelectedId().charAt(0));
+	@NiftyEventSubscriber(id="loadDropDown")
+	public void onLoadDropDown(final String id, final DropDownSelectionChangedEvent event){
+		this.loadFactor = nifty.getScreen("factor").findNiftyControl(id, DropDown.class).getSelectedIndex();
 	}
 
-	@NiftyEventSubscriber(id="passangerRadioButtonGroup")
-	public void onPassangerRadioButtonGroup(final String id, final RadioButtonGroupStateChangedEvent event){
-		this.passangerFactor = Character.getNumericValue(event.getSelectedId().charAt(0));
+	@NiftyEventSubscriber(id="passangerDropDown")
+	public void onPassangerDropDown(final String id, final DropDownSelectionChangedEvent event){
+		this.passangerFactor = nifty.getScreen("factor").findNiftyControl(id, DropDown.class).getSelectedIndex();
 	}
 
-	@NiftyEventSubscriber(id="placeKnowledgeRadioButtonGroup")
-	public void onPlaceKnowledgeRadioButtonGroup(final String id, final RadioButtonGroupStateChangedEvent event){
-		this.placeKnowledgeFactor = Character.getNumericValue(event.getSelectedId().charAt(0));
+	@NiftyEventSubscriber(id="placeKnowledgeDropDown")
+	public void onPlaceKnowledgeDropDown(final String id, final DropDownSelectionChangedEvent event){
+		this.placeKnowledgeFactor = nifty.getScreen("factor").findNiftyControl(id, DropDown.class).getSelectedIndex();
 	}
 
-	@NiftyEventSubscriber(id="routeDurationRadioButtonGroup")
-	public void onRouteDurationRadioButtonGroup(final String id, final RadioButtonGroupStateChangedEvent event){
-	this.routeDurationFactor = Character.getNumericValue(event.getSelectedId().charAt(0));
+	@NiftyEventSubscriber(id="routeDurationDropDown")
+	public void onRouteDurationDropDown(final String id, final DropDownSelectionChangedEvent event){
+	this.routeDurationFactor = nifty.getScreen("factor").findNiftyControl(id, DropDown.class).getSelectedIndex();
 	}
 
-	@NiftyEventSubscriber(id="signRadioButtonGroup")
-	public void onSignRadioButtonGroup(final String id, final RadioButtonGroupStateChangedEvent event){
-		this.signFactor = Character.getNumericValue(event.getSelectedId().charAt(0));
+	@NiftyEventSubscriber(id="signDropDown")
+	public void onSignDropDown(final String id, final DropDownSelectionChangedEvent event){
+		this.signFactor = nifty.getScreen("factor").findNiftyControl(id, DropDown.class).getSelectedIndex();
 	}
 
-	@NiftyEventSubscriber(id="temperatureRadioButtonGroup")
-	public void onTemperatureRadioButtonGroup(final String id, final RadioButtonGroupStateChangedEvent event){
-		this.temperatureFactor = Character.getNumericValue(event.getSelectedId().charAt(0));
+	@NiftyEventSubscriber(id="temperatureDropDown")
+	public void onTemperatureDropDown(final String id, final DropDownSelectionChangedEvent event){
+		this.temperatureFactor = nifty.getScreen("factor").findNiftyControl(id, DropDown.class).getSelectedIndex();
 	}
 
-	@NiftyEventSubscriber(id="timeRadioButtonGroup")
-	public void onTimeRadioButtonGroup(final String id, final RadioButtonGroupStateChangedEvent event){
-		this.timeFactor = Character.getNumericValue(event.getSelectedId().charAt(0));
+	@NiftyEventSubscriber(id="timeDropDown")
+	public void onTimeDropDown(final String id, final DropDownSelectionChangedEvent event){
+		this.timeFactor = nifty.getScreen("factor").findNiftyControl(id, DropDown.class).getSelectedIndex();
 	}
 
-	@NiftyEventSubscriber(id="trafficIntensityRadioButtonGroup")
-	public void onTrafficIntensityRadioButtonGroup(final String id, final RadioButtonGroupStateChangedEvent event){
-		this.trafficIntensityFactor = Character.getNumericValue(event.getSelectedId().charAt(0));
+	@NiftyEventSubscriber(id="trafficIntensityDropDown")
+	public void onTrafficIntensityDropDown(final String id, final DropDownSelectionChangedEvent event){
+		this.trafficIntensityFactor = nifty.getScreen("factor").findNiftyControl(id, DropDown.class).getSelectedIndex();
 	}
 
-	@NiftyEventSubscriber(id="volumneRadioButtonGroup")
-	public void onVolumneRadioButtonGroup(final String id, final RadioButtonGroupStateChangedEvent event){
-		this.volumneFactor = Character.getNumericValue(event.getSelectedId().charAt(0));
+	@NiftyEventSubscriber(id="volumneDropDown")
+	public void onVolumneDropDown(final String id, final DropDownSelectionChangedEvent event){
+		this.volumneFactor = nifty.getScreen("factor").findNiftyControl(id, DropDown.class).getSelectedIndex();
 	}
 
-	@NiftyEventSubscriber(id="weatherRadioButtonGroup")
-	public void onWeatherRadioButtonGroup(final String id, final RadioButtonGroupStateChangedEvent event){
-		this.weatherFactor = Character.getNumericValue(event.getSelectedId().charAt(0));
+	@NiftyEventSubscriber(id="weatherDropDown")
+	public void onWeatherDropDown(final String id, final DropDownSelectionChangedEvent event){
+		this.weatherFactor = nifty.getScreen("factor").findNiftyControl(id, DropDown.class).getSelectedIndex();
 	}
 	
 	
 	
 	@Override
 	public void bind(Nifty nifty, Screen screen) {
-		// TODO Auto-generated method stub
-		
+		//populate the dropDownMenus
+		DropDown dropdown = screen.findNiftyControl("brightnessDropDown", DropDown.class);
+		dropdown.addItem("Daylight");
+		dropdown.addItem("Night");
+		dropdown.addItem("Dawn");
 	}
 
 	@Override
